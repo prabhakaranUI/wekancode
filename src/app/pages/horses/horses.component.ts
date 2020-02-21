52b94373dcc4a5a9c1884125de5174c3f59478e1
kmt-prabhakaran <prabha@26>
2020-02-21 22:47:09 +0530

    commit

--- src/app/pages/horses/horses.component.ts
+++ src/app/pages/horses/horses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "../../shared/login.service";
+import {DialogComponent} from "./dialog/dialog.component";
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-horses',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horses.component.scss']
 })
 export class HorsesComponent implements OnInit {
+  constructor(public login_service: LoginService, public dialog: MatDialog) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+ this.getList();
+  }
+
+
+    horseDialog(): void {
+        const dialogRef = this.dialog.open(DialogComponent, {
+            width: '250px'
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
+
+
+  ////list////
+  getList(){
+      this.login_service.list().subscribe(
+          (successData) => {
+            this.loginDataSuccess(successData);
+          },
+          (error) => {
+            this.loginDataFailure(error);
+          });
+    }
+
+  public loginDataSuccess(successData){
+    console.log(successData, 'horses');
+  }
+  public loginDataFailure(error){
+    console.log(error)
   }
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import {LoginService} from "../../shared/login.service";
 import {MatSnackBar} from '@angular/material/snack-bar';
 import * as sha512 from 'js-sha512';
 import {AuthService} from "../../shared/auth.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -13,7 +14,7 @@ import {AuthService} from "../../shared/auth.service";
 })
 export class LoginComponent implements OnInit {
   public login: FormGroup;
-  constructor(public fb: FormBuilder, public login_service: LoginService, private _snackBar: MatSnackBar, public auth: AuthService) {
+  constructor(public fb: FormBuilder, public login_service: LoginService, public router: Router, private _snackBar: MatSnackBar, public auth: AuthService) {
 
     this.login = this.fb.group({
       'email': ['tamilselvan@mailinator.com', Validators.compose([Validators.required])],
@@ -22,6 +23,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+      this.auth.checkAuthentication();
   }
 
 
@@ -39,23 +41,21 @@ export class LoginComponent implements OnInit {
             'email': value.email,
             'password': sha512.sha512(value.password)
         };
-       this.login_service.getLogin(data).subscribe(
+       this.login_service.doLogin(data).subscribe(
            (successData) => {
              this.loginDataSuccess(successData);
            },
           (error) => {
               this.loginDataFailure(error);
-        }
-       )
-
+        });
     }
   }
 
    public loginDataSuccess(successData){
+       console.log(successData);
        this.openSnackBar('Login Successful', 'ok');
-       this.auth.setSessionData('access_token', successData.data.access_token)
-      console.log(successData)
-
+       this.auth.setSessionData('access_token', successData.data.access_token);
+       this.router.navigate(['/horses']);
    }
     public loginDataFailure(error){
         console.log(error)
